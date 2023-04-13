2c2f5fcda7661af08d4ea52faf39f27bb19c09de
Austin Joiner <austin.j.joiner1@gmail.com>
2023-04-13 14:40:21 -0400

    added in isNumeric if statement

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,21 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
 public class Main {
 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input here: ");
+        String inputHere = input.nextLine();
+
+        System.out.println("You entered in " + inputHere + ".");
+
+        if(StringUtils.isNumeric(inputHere)) {
+            System.out.println("You typed in a number!");
+        } else {
+            System.out.println("This is not a number.");
+        }
+
+    }
 }
